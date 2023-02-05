fca02e7dcfcdb477f30b81a851244586a2ff0218
edwardmiller123 <edwardomiller123@gmail.com>
2023-02-05 13:57:37 +0000

    Add action ordering and fix actions

--- main.cpp
+++ main.cpp
@@ -1,65 +1,97 @@
 #include <iostream>
-
+#include <random>
 #include <string>
 #include <vector>
 
-class character {
-  public:
-    std::string name;
-    int hp;
-    int baseSpeed;
-    int baseAttack;
-    int speed;
-    int attack;
-    int damageInflicted;
-    int player;
-    bool guarding;
-    bool preparingToDodge;
+class character
+{
+public:
+  std::string name;
+  int hp;
+  int baseSpeed;
+  int baseAttack;
+  int speed;
+  int attack;
+  int player;
+  bool guarding;
+  bool preparingToDodge;
 
-    void new_character(std::string newName, int newHp, int newSpeed, int newAttack, bool initGuard = false, bool initDodge = false) {
-      name = newName;
-      hp = newHp;
-      baseSpeed = newSpeed;
-      baseAttack = newAttack;
-      guarding = initGuard;
-      preparingToDodge = initDodge;
-    }
+  void new_character(std::string newName, int newHp, int newSpeed, int newAttack, bool initGuard = false, bool initDodge = false)
+  {
+    name = newName;
+    hp = newHp;
+    baseSpeed = newSpeed;
+    baseAttack = newAttack;
+    guarding = initGuard;
+    preparingToDodge = initDodge;
+  }
 
-    void light_attack() {
-      int *attackPtr = &attack;
-      int *speedPtr = &speed;
-      *attackPtr = baseAttack - 5;
-      *speedPtr = baseSpeed + 15;
-    }
+  void light_attack()
+  {
+    int *attackPtr = &attack;
+    int *speedPtr = &speed;
+    *attackPtr = baseAttack - 5;
+    *speedPtr = baseSpeed + 15;
+  }
 
-    void heavy_attack() {
-      int *attackPtr = &attack;
-      int *speedPtr = &speed;
-      *attackPtr = baseAttack + 10;
-      *speedPtr = baseSpeed - 20;
-    }
+  void heavy_attack()
+  {
+    int *attackPtr = &attack;
+    int *speedPtr = &speed;
+    *attackPtr = baseAttack + 10;
+    *speedPtr = baseSpeed - 20;
+  }
 
-    void dodge() {
-      
-    }
+  void dodge()
+  {
+    bool *dodgePtr = &preparingToDodge;
+    int *speedPtr = &speed;
+    *dodgePtr = true;
+    *speedPtr = baseSpeed + 35;
+  }
 
-    void guard() {
-      
-    }
-
+  void guard()
+  {
+    bool *guardPtr = &guarding;
+    *guardPtr = true;
+  }
 };
 
 character speedy_mc_speed, big_boi, allrounder;
 
-void resetStats(std::vector<character> combatants) {
-  for (int i = 0; i < 2; i++) {
-    character* playerPtr = &combatants[i];
-    // playerPtr.attack = 0;
-    // playerPtr.speed = 0;
+void resetStats(std::vector<character> combatants)
+{
+  for (int i = 0; i < 2; i++)
+  {
+    int *attackPtr = &combatants[i].attack;
+    int *speedPtr = &combatants[i].speed;
+    bool *dodgePtr = &combatants[i].preparingToDodge;
+    *attackPtr, *speedPtr = 0;
+    *dodgePtr = false;
   }
 }
 
-int main() {
+void calculateOutcome(std::vector<character> combatants)
+{
+  std::vector<character> actionOrder;
+  int damageInflicted;
+  if (combatants[0].speed >= combatants[1].speed)
+  {
+    actionOrder.push_back(combatants[0]);
+    actionOrder.push_back(combatants[1]);
+  }
+  else
+  {
+    actionOrder.push_back(combatants[1]);
+    actionOrder.push_back(combatants[0]);
+  }
+  for (int n = 0; n < 2; n++) {
+
+  }
+}
+
+int main()
+{
   speedy_mc_speed.new_character("SpeedyMcSpeed", 45, 85, 10);
   big_boi.new_character("BigBoi", 90, 30, 20);
   allrounder.new_character("Joe", 60, 60, 15);
@@ -82,43 +114,54 @@ int main() {
 
   std::vector<std::string> players = {choicePlayer1, choicePlayer2};
 
-  for (int j = 0; j < players.size(); j++) {
-    for (int i = 0; i < characters.size(); i++) {
-      if (players[j] == characters[i].name) {
+  for (int j = 0; j < players.size(); j++)
+  {
+    for (int i = 0; i < characters.size(); i++)
+    {
+      if (players[j] == characters[i].name)
+      {
         combatants.push_back(characters[i]);
         // combatants[j].player = j;
       }
-    } 
+    }
   }
-
 
   std::cout << "\nBegin!\n";
 
-  while (victory != true) {
+  while (victory != true)
+  {
 
-    // resetStats(combatants);
+    resetStats(combatants);
 
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++)
+    {
       std::cout << "Player " << i + 1 << ": Choose action...\n";
       std::cin >> action;
 
-      switch (action) {
-        case 1:
-          combatants[i].light_attack();
-          break;
-        case 2:
-          combatants[i].heavy_attack();
-          break;
-        case 3: 
-          combatants[i].dodge();
-          break;
-        case 4:
-          combatants[i].guard();
-          break;  
+      switch (action)
+      {
+      case 1:
+        combatants[i].light_attack();
+        break;
+      case 2:
+        combatants[i].heavy_attack();
+        break;
+      case 3:
+        combatants[i].dodge();
+        break;
+      case 4:
+        combatants[i].guard();
+        break;
       }
-    } 
+    }
+    calculateOutcome(combatants);
     victory = true;
   }
-  std::cout << combatants[0].attack << "\n";
-  std::cout << combatants[1].attack << "\n";
+  // std::cout << combatants[0].name << "\n";
+  // std::cout << combatants[0].attack << "\n";
+  // std::cout << combatants[0].speed << "\n";
+  // std::cout << "=======================\n";
+  // std::cout << combatants[1].name << "\n";
+  // std::cout << combatants[1].attack << "\n";
+  // std::cout << combatants[1].speed << "\n";
 }
